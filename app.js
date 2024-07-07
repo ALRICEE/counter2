7eb5d8d2921b0204f92d7be70674050eb184db29
ALRICEE <alivia.rice.pht@gmail.com>
2024-07-07 09:23:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(express.text());
 app.use(cors());
